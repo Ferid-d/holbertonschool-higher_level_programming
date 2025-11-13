9ea8b171a2cddd58415febb029ac5826da00c498
Ferid-d <fmeybullayev.com>
2025-11-13 12:26:57 +0000

    gvegv

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+first = word[0:2]
+last = word[-2:]
+middle = word[2:-2]
+print(f"First 3 letters: {first}")
+print(f"Last 2 letters: {last}")
+print(f"Middle word: {middle}")
