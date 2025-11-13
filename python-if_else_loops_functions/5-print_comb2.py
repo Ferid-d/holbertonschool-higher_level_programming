3ddb0167810967e091652343b4335eda788dcc4b
Ferid-d <fmeybullayev.com>
2025-11-13 13:25:17 +0000

    dfvfv

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(0,100):
+for i in range(0, 100):
     if i < 10:
-        print(f""0"+{i}", end=",")
+        print("0{}".format(i), end=", ")
     else:
-        ptint(i, end=",")
+        print("{}".format(i), end=", ")
