8b6c38047c4edc190754a58a7a8ff29fea650457
Ferid-d <fmeybullayev.com>
2025-11-13 14:05:54 +0000

    dfvfv

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def isupper(c):
-    return 97 <= ord(c) <= 122
+    # Check if character is uppercase
+    return 65 <= ord(c) <= 90
+
+def uppercase(str):
+    for char in str:
+        if not isupper(char) and 97 <= ord(char) <= 122:
+            print("{}".format(chr(ord(char) - 32)), end="")
+        else:
+            print("{}".format(char), end="")
+    print()
