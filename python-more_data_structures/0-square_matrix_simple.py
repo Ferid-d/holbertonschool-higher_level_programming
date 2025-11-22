3c5e2811977b9c11af19c146adf04c82c72ca128
= <=>
2025-11-22 16:52:29 +0000

    wfc

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -5,5 +5,7 @@ in a 2D matrix.
 
 
 def square_matrix_simple(matrix=[]):
-    """Return a new matrix with the square of all values of the input matrix."""
+    """Return a new matrix with the square of all values of the input
+    matrix.
+    """
     return [[value ** 2 for value in row] for row in matrix]
