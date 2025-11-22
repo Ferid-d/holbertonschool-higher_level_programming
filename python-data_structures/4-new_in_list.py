8d4dcb06ef3513d114b7b911cd51e40785696261
= <=>
2025-11-22 10:14:15 +0000

    ex

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
-        new_list[idx] = element
+        return new_list
+    new_list[idx] = element
     return my_list
-    return new_list
 
 
 if __name__ == "__main__":
+    idx = 3
+    element = 9
     my_list = [1,2,3,4,5]
+    new_list = my_list
     new_in_list(my_list)
 
