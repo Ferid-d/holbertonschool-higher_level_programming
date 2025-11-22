086f56f86c8e1d447fd0e547be723fda02e57cd7
= <=>
2025-11-22 09:56:37 +0000

    ex

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,10 +2,10 @@
 def print_reversed_list_integer(my_list=[]):
     a = len(my_list)
     for i in range(a):
-        c = my_list[a-i-1]
+        c = my_list[a - 1 - i]
         print("{:d}".format(c))
 
 
 if __name__ == "__main__":
-    my_list = [1,2,3,4,5]
+    my_list = [1, 2, 3, 4, 5]
     print_reversed_list_integer(my_list)
