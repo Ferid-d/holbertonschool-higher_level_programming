137d9c99da55db19662a446dd0f272e38f4b26c9
= <=>
2025-11-22 07:06:57 +0000

    g

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-def add(a, b):
-    return a + b
-def sub(a, b):
-    return a - b
-def mul(a, b):
-    return a * b
-def div(a, b):
-    return a / b
+from calculator_1 import add, sub, mul, div
+
 if __name__ == "__main__":
     a = 10
     b = 5
