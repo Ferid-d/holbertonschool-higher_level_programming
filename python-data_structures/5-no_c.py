f9f073b7e6d5b24d0bd25e301dd9683d212b128b
= <=>
2025-11-22 16:29:26 +0000

    sxc

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""Module that defines no_c function."""
+
+
 def no_c(my_string):
+    """Return a copy of my_string with all 'c' and 'C' removed."""
     new_string = ""
     for ch in my_string:
         if ch != 'c' and ch != 'C':
             new_string += ch
     return new_string
-
