f429b87e0d53757e8b9c746bfeeefac79200f635
= <=>
2025-11-22 15:38:39 +0000

    sc

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
+    # siyahının surətini yarat
+    new_list = my_list[:]
+    if idx < 0 or idx >= len(new_list):
         return new_list
     new_list[idx] = element
-    return my_list
+    return new_list
 
 
 if __name__ == "__main__":
-    idx = 3
-    element = 9
-    my_list = [1,2,3,4,5]
+    my_list = [1, 2, 3]
+    idx = 1
+    element = 4
     new_list = new_in_list(my_list, idx, element)
-    print(new_list)
-    print(my_list)
-
+    print(new_list)  # [1, 4, 3]
+    print(my_list)   # [1, 2, 3]
