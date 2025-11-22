d168c9be26926138bc8e130558ed978cfb47680e
= <=>
2025-11-22 10:16:26 +0000

    ex

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
     idx = 3
     element = 9
     my_list = [1,2,3,4,5]
-    new_list = my_list
-    new_in_list(my_list)
+    new_list = new_in_list(my_list, idx, element)
+    print(new_list)
+    print(my_list)
 
