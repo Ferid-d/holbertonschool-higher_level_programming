3c3f88edea743b6decf328dfaf431598c6c5f035
= <=>
2025-11-22 06:51:39 +0000

    g

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-def function(a,b):
-    print("a + b = {}".format(a + b))
+from add_0 import add
+
 if __name__ == "__main__":
-    function(1,2)
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
