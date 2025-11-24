79b2f1ad6f2e88113a11326598e12ddad26df5f2
= <=>
2025-11-24 11:57:46 +0000

    Wf

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -48,7 +48,3 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join(["#" * self.__width for _ in range(self.__height)])
-
-    def __repr__(self):
-        """Returns a string representation to recreate the object."""
-        return f"{self.__class__.__name__}({self.__width}, {self.__height})"
