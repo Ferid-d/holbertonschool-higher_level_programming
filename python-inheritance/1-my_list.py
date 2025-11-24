7b66b36daba4e7b6879ad979587416ac6d9f4a44
= <=>
2025-11-24 12:58:39 +0000

    sc

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
-"""Module that defines MyList class.
+"""Module that defines a MyList class inheriting from list."""
 
->>> my_list = MyList()
->>> my_list.append(1)
->>> my_list.append(4)
->>> my_list.append(2)
->>> my_list.append(3)
->>> my_list.append(5)
->>> my_list.print_sorted()
-[1, 2, 3, 4, 5]
-"""
+
 class MyList(list):
-    """Custom list that can print itself sorted."""
+    """Represents a list with a method to print it sorted."""
 
     def print_sorted(self):
         """Prints the list in ascending order without modifying it."""
