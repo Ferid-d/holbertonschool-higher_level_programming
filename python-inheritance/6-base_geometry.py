ff9c6e7bec2a92fa2bdcdfe5c3ea91003d14db4d
= <=>
2025-11-24 13:05:53 +0000

    sc

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Defines class BaseGeometry based on 5-base_geometry.py
+"""
+
+
+class BaseGeometry:
+    """Base class"""
+
+    def area(self):
+        """Raises an exception because area is not implemented"""
+        raise Exception("area() is not implemented")
