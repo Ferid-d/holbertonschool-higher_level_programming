ca0e6e23ac7e98f648f3c6ab9e3e03429575b3f5
= <=>
2025-11-24 12:10:27 +0000

    sc

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module that defines a lookup function to return all attributes of an object."""
 
+
 def lookup(obj):
     """Returns a list of available attributes and methods of an object."""
     return dir(obj)
