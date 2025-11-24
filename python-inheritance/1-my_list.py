61ef679f03d0c76776d1088335807ac91dfa5327
= <=>
2025-11-24 12:23:20 +0000

    sc

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-"""Module that defines a MyList class inheriting from list."""
+"""Module that defines a MyList class inheriting from list.
 
+>>> my_list = MyList()
+>>> my_list.append(1)
+>>> my_list.append(2)
+>>> my_list.print_sorted()
+[1, 2]
+"""
 
 class MyList(list):
     """Represents a list with a method to print it sorted."""
