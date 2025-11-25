b1f41baf51939a83fe9adaad6a666ccd71d92433
= <=>
2025-11-25 13:12:18 +0000

    svc

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module for to_json_string function."""
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Return the JSON representation of an object as a string.
+    """
+    return json.dumps(my_obj)
