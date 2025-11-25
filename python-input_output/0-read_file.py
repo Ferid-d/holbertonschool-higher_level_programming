cddb2494a816224f60d3382e6b3ad65e83706f02
= <=>
2025-11-25 13:07:59 +0000

    svc

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module for read_file function."""
 
+
 def read_file(filename=""):
     """Reads a UTF-8 text file and prints its contents to stdout."""
     with open(filename, encoding="utf-8") as f:
