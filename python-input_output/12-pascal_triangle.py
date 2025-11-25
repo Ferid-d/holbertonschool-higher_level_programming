089acdbf43b0ab138ad0b8509e3c6e4431fce965
= <=>
2025-11-25 13:21:46 +0000

    svc

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module for generating Pascal's triangle."""
+
+
+def pascal_triangle(n):
+    """
+    Return a list of lists of integers representing Pascal's triangle of n.
+
+    Args:
+        n (int): Number of rows in the triangle.
+
+    Returns:
+        list: Pascal's triangle as a list of lists.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        prev_row = triangle[i - 1]
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
