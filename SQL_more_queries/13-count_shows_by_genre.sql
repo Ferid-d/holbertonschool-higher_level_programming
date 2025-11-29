eaf68e7792311da489ce805fa7d03c953acaac51
= <=>
2025-11-29 17:06:27 +0000

    erfc

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- jbnb
 SELECT g.name AS genre, COUNT(sg.show_id) AS number_of_shows
-FROM genres g
+FROM tv_genres g
 JOIN tv_show_genres sg ON g.id = sg.genre_id
 GROUP BY g.name
 ORDER BY number_of_shows DESC;
