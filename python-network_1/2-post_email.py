65358e418771ecc936a6ab73e5aed122ab415f1e
= <=>
2025-12-01 11:10:45 +0000

    fc

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
-"""Sends POST request with email parameter and displays response"""
+"""Sends POST request with email"""
 
 import sys
 import urllib.request
 import urllib.parse
 
 if __name__ == "__main__":
-    # Get URL and email from command line arguments
     url = sys.argv[1]
     email = sys.argv[2]
-    
-    # Prepare the data to send
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-    
-    # Create POST request
+    data = urllib.parse.urlencode({'email': email}).encode()
     req = urllib.request.Request(url, data=data)
-    
-    # Send request and get response
     with urllib.request.urlopen(req) as response:
-        body = response.read()
-        print(body.decode('utf-8'))
+        print(response.read().decode())
