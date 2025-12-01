5ff352887bf85d8d5a49c98f02823c370d92699f
= <=>
2025-12-01 11:31:34 +0000

    fc

---
+++ python-network_1/3-error_code.py
@@ -0,0 +1,12 @@
+import sys
+import urllib.request
+import urllib.error
+
+if __name__ == "__main__":
+    url = sys.argc[1]
+    try:
+        with urllib.request.urlopen('url') as response
+        body = response.read()
+	print(body.decode('utf-8')
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
