512dbb8aa9383114004877e2866c062bd6f7f5d4
= <=>
2025-12-01 11:34:26 +0000

    fc

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     url = sys.argc[1]
     try:
         with urllib.request.urlopen('url') as response:
-        body = response.read()
-	print(body.decode('utf-8')
+            body = response.read()
+            print(body.decode('utf-8')
     except urllib.error.HTTPError as e:
         print(f"Error code: {e.code}")
