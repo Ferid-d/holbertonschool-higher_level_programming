83fb5da38299d044f2697a3cfee922ad1d5c96b1
= <=>
2025-12-01 10:46:15 +0000

    fc

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+import sys
+import urllib.request
+
+if __name__ == "__main__":
+    url = sys.argv[0]
+    with urllib.request.urlopen(url) as response:
+        x-request-id = response.getheader('X-Request-Id')
+    if x-request-id:
+        print(x-request-id)
