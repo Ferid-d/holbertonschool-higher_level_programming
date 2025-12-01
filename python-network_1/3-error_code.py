693febce1c44286c162c84e04386753a4eab2296
= <=>
2025-12-01 11:33:05 +0000

    fc

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Fetches https://intranet.hbtn.io/status using urllib package"""
 import sys
 import urllib.request
 import urllib.error
@@ -6,7 +7,7 @@ import urllib.error
 if __name__ == "__main__":
     url = sys.argc[1]
     try:
-        with urllib.request.urlopen('url') as response
+        with urllib.request.urlopen('url') as response:
         body = response.read()
 	print(body.decode('utf-8')
     except urllib.error.HTTPError as e:
